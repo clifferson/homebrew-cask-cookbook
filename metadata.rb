bf2862757710695a9822d3330ebe419b84eb8102
Cliff Erson <cerson@me.com>
2013-08-16 22:53:53 -0700

    make it work

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-name             'homebrew-cask-cookbook'
-description      'Installs/Configures homebrew-cask-cookbook'
+name             'homebrew'
+description      'Installs/Configures homebrew-cask'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'sprout-osx-apps'
---
+++ recipes/cask.rb
@@ -0,0 +1,12 @@
+include_recipe "sprout-osx-base::homebrew"
+
+directory "/opt/homebrew-cask" do
+  owner node['current_user']
+end
+
+execute "tap phinze/homebrew-cask" do
+  command "brew tap phinze/homebrew-cask"
+  not_if { system("brew tap | grep 'cask' > /dev/null 2>&1") }
+end
+
+brew "brew-cask"
