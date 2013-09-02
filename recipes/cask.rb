1836e835d2c2957c3414ee3c0627ae1145a37d67
Cliff Erson <cerson@me.com>
2013-09-02 12:14:52 -0700

    change perms on caskroom dir to current_user

--- recipes/cask.rb
+++ recipes/cask.rb
@@ -4,6 +4,10 @@ directory "/opt/homebrew-cask" do
   owner node['current_user']
 end
 
+directory "/opt/homebrew-cask/Caskroom" do
+  owner node['current_user']
+end
+
 execute "tap phinze/homebrew-cask" do
   command "brew tap phinze/homebrew-cask"
   not_if { system("brew tap | grep 'cask' > /dev/null 2>&1") }
